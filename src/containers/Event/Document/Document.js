82e906b617d83ee466583bb4e10562ae22411bf7
Pietro Rampazzo <pietro@rampazzo.co>
2020-04-04 21:36:34 +0200

    #3 improve iframe height

--- src/containers/Event/Document/Document.js
+++ src/containers/Event/Document/Document.js
@@ -3,13 +3,14 @@ import "./Document.css";
 
 function Document() {
   return (
-    <iframe
-      title="document"
-      src="https://docs.google.com/presentation/d/e/2PACX-1vS-8ObYLMmB5QarJRM34yiDiUcGwHXdDfXQtrzpdEdwIM-QyU_3mehDpSu6l68p1BdneEHkp-_YC23E/embed?start=false&loop=false&delayms=60000"
-      frameBorder="0"
-      allowFullScreen={false}
-      className={"Document-frame"}
-    ></iframe>
+    <div className={"Document-frame"}>
+      <iframe
+        title="document"
+        src="https://docs.google.com/presentation/d/e/2PACX-1vS-8ObYLMmB5QarJRM34yiDiUcGwHXdDfXQtrzpdEdwIM-QyU_3mehDpSu6l68p1BdneEHkp-_YC23E/embed?start=false&loop=false&delayms=60000"
+        frameBorder="0"
+        allowFullScreen={false}
+      ></iframe>
+    </div>
   );
 }
 
