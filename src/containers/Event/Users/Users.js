55f0de20b09d4e9d783640fca733bb27864267b0
Pietro Rampazzo <pietro@rampazzo.co>
2020-04-08 13:56:35 +0200

    #2 add follow-me button

--- src/containers/Event/Users/Users.js
+++ src/containers/Event/Users/Users.js
@@ -14,11 +14,18 @@ import {
   Domain as DomainIcon,
   EmojiEmotions as EmojiIcon,
 } from "@material-ui/icons";
+
+import ExploreIcon from "@material-ui/icons/Explore";
+import ExploreOffIcon from "@material-ui/icons/ExploreOff";
+
 import * as FirestoreService from "../../../services/firestore";
 import ErrorMessage from "../../../components/ErrorMessage/ErrorMessage";
 
 const toggleIsMentor = (u, eventId) =>
   FirestoreService.setUserIsMentor(u.userId, eventId, !u.isMentor);
+
+const followMe = (u, room, eventId) =>
+  FirestoreService.addUserToRoom(u.userId, room, eventId);
 
 function Users({
   user,
@@ -29,6 +36,9 @@ function Users({
   eventRooms,
   eventRoomsUsers,
 }) {
+  // get user room id
+  const myRoom = eventRoomsUsers.find((u) => u.userId === user.userId);
+
   // map users to room
   const usersWithRoom = eventUsers.map((u) => {
     const userRoom = eventRoomsUsers.find((ru) => ru.userId === u.userId);
@@ -56,6 +66,7 @@ function Users({
               <TableCell>Room</TableCell>
               <TableCell>Type</TableCell>
               <TableCell align="right">Actions</TableCell>
+              <TableCell align="right">Follow Me</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -78,6 +89,21 @@ function Users({
                       placement="bottom"
                     >
                       {u.isMentor ? <EmojiIcon /> : <DomainIcon />}
+                    </Tooltip>
+                  </IconButton>
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton
+                    aria-label="promote"
+                    color="primary"
+                    onClick={() => followMe(u, myRoom.roomId, event.eventId)}
+                    // disabled={myRoom.roomId === u.roomId}
+                  >
+                    <Tooltip
+                      title={u.isMentor ? "Follow me" : "Same room"}
+                      placement="bottom"
+                    >
+                      {u.isMentor ? <ExploreIcon /> : <ExploreOffIcon />}
                     </Tooltip>
                   </IconButton>
                 </TableCell>
